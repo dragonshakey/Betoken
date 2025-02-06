55c2f8ea873ea067642a8d79bdb430b54658615d
dragonshakey <dragonshakey@gmail.com>
2025-02-06 10:18:57 +0200

    connect UI elements to the code

--- app/src/main/java/kaplan/shaked/betoken/activity_profile.java
+++ app/src/main/java/kaplan/shaked/betoken/activity_profile.java
@@ -1,6 +1,8 @@
 package kaplan.shaked.betoken;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class activity_profile extends AppCompatActivity {
 
+    TextView email, gender, aboutMe;
+    ImageView profilePicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        email = findViewById(R.id.textViewProfileEmail);
+        gender = findViewById(R.id.textViewProfileGender);
+        aboutMe = findViewById(R.id.textViewProfileAboutMe);
+        profilePicture = findViewById(R.id.imageViewProfileProfilePicture);
     }
 }
