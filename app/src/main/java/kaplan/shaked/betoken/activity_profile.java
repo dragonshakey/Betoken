8ccc930c33fbb1403be55b90493d23c08e06f49a
dragonshakey <dragonshakey@gmail.com>
2025-02-06 10:07:01 +0200

    Add a profile activity with all relevant UI

---
+++ app/src/main/java/kaplan/shaked/betoken/activity_profile.java
@@ -0,0 +1,19 @@
+package kaplan.shaked.betoken;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class activity_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
